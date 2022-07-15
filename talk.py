3bbfd351f6e2fe344bbde0fe6dfbcaffb017463c
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2022-07-15 15:46:38 +0300

    Create talk.py

---
+++ talk.py
@@ -0,0 +1,23 @@
+def talk(text):
+    if text.startswith("talk('"):
+        if text.endswith("');"):
+            text = text.replace("talk('","")
+            text = text.replace("');","")
+            print(text)
+        else:
+            print('  File "<stdin>", line 1')
+            print('    '+text)
+            print('    '+text.replace(text,' ')+'^')
+            print('SyntaxError: EOL while scanning string literal')
+    else:
+        if text.startswith('#'):
+            pass
+        else:
+            print('Traceback (most recent call last):')
+            print(' File "<stdin>", line 1, in <module>')
+            print("NameError: name '"+text+"' is not defined")
+
+
+while True:
+    text = input('talk >>>')
+    talk(text)
